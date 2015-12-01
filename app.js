fef832b372977b997e5fc4c2243d372213d25697
Jens Bodal <bodalj@oregonstate.edu>
2015-12-01 08:01:11 +0000

    now have one-time-code ...

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Pass all routes to routes file
 app.use('/', routes);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,42 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/step:number', function(req, res) {
-    res.render('step' + req.params.number);
+    var stepNumber = req.params.number;
+    try {
+        res.render('step' + stepNumber, {title: 'Step #' + stepNumber});
+    }
+    catch(err) {
+        res.send("NONONONONON");
+    }
 });
+
+router.get('/jensbodal', function(req, res) {
+    res.send("This page does not exist");
+});
+
+router.get('/one-time-code', function(req, res, next) {
+    var context = getContext('GET', req, false);
+    var state = context.params[0].value; // we should verify this, but we don't
+    var oneTimeCode = context.params[1].value;
+    res.render('step1', {pageData: [state, oneTimeCode]});
+    next();
+});
+
+function getContext(type, req, isPost) {
+    var params = [];
+    if (isPost) {
+        reqType = req.body;
+    }
+    else {
+        reqType = req.query;
+    }
+    for (var param in reqType) {
+        params.push({'name':param, 'value':reqType[param]});
+    }
+    var context = {};
+    context.params = params;
+    context.type = type;
+    return context;
+}
 
 module.exports = router;
